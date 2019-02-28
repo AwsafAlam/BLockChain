217df88f048054d18bdc54ad8237f3e0c201eee7
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-02-28 22:47:27 +0600

    Simple Blockshain

---
+++ SimpleBlockChain/simpleChain.py
@@ -0,0 +1,12 @@
+# Creating a basic blockchain in python using the SHA-1 hashing algorithm
+
+class Block(object):
+    def __init__(self, *args):
+        super(Block, self).__init__(*args)
+
+
+class BlockChain(object):
+    def __init__(self, *args):
+        super(BlockChain, self).__init__(*args)
+        
+        
