160d5956d107d8d029aeec4cd4ca8c03449af2fa
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-03-06 23:57:28 +0600

    Basic blockchain structure

--- CryptoCurrency/main.js
+++ CryptoCurrency/main.js
@@ -1,6 +1,27 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
 
     constructor(index, timestamp , data , previoushash=''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previoushash = previoushash;
+        this.hash = this.calculateHash();
+    }
 
+    calculateHash(){
+        return SHA256(this.index + this.previoushash+ JSON.stringify(this.data)).toString();
+    }
+}
+
+class BlockChain{
+
+    constructor(){
+        this.chain = [this.createGenesisBLock()];
+    }
+
+    createGenesisBLock(){
+        return new Block(0,'02/03/2019',"Genesis","0");
     }
 }
